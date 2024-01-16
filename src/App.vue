a627c83304c41478306b223e2f7c676ec7e5c072
ilfey <ilfey.global@gmail.com>
2024-01-16 14:24:10 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <!-- header here -->
+
+  <main class="flex-1 py-4 w-full">
+
+    <HelloWorld msg="Vite + Vue" />
+
+  </main>
+
+  <footer class="py-4 w-full">
+    <p class="text-center text-xs font-bold text-gray-500">&copy; Created by ilfey 2024</p>
+  </footer>
+</template>
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+import { createHtmlPlugin } from "vite-plugin-html";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    createHtmlPlugin({
+      minify: true,
+      viteNext: true,
+    }),
+  ],
+});
